a6ad0212d4e165c6830aefd399334abb35b0380c
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-03-26 05:44:26 -0700

    Create leftshift.c

---
+++ leftshift.c
@@ -0,0 +1,16 @@
+//An integer variable num contains 2 as its value  Print the content s of num after a left shift operation
+
+
+#include <stdio.h>
+
+int main() {
+    int num = 2;
+
+  
+    num = num << 1;
+
+
+    printf("Content of num after left shift operation: %d\n", num);
+
+    return 0;
+}
