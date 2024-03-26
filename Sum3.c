565cad863fd5a4031cd45d2168b87d5f01050764
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-03-26 04:56:43 -0700

    Create Sum3.c

---
+++ Sum3.c
@@ -0,0 +1,28 @@
+
+
+
+#include<stdio.h>
+
+
+
+int main(){
+
+
+
+ int no, no1,no2;
+int sum;
+ printf("enter the no to be check:");
+ scanf("%d\n",&no);
+  printf("enter the no to be check:");
+ scanf("%d\n",&no1);
+  printf("enter the no to be check:");
+ scanf("%d\n",&no2);
+
+
+
+
+    sum=no+no1+no2;
+
+printf(" sum of the three numbers is %d", sum);
+   return 0;
+}
