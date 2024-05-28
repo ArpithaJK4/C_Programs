d3b70c3193ac9d2124829db717b96bba55595844
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-05-28 11:10:34 -0700

    Add files via upload

---
+++ HELLO.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+clrscr();
+textcolor(RED);
+textbackground(WHITE);
+
+for(i=1;i<=100;i++){
+
+cprintf(" i love you   \3");
+
+delay(60);
+}
+getch();
+}
