bdcdd6fa898832c9581adb6e2983080bd382e07a
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-12-20 10:11:38 +0530

    Create pattern4.c

---
+++ pattern4.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+ int n;
+ scanf("%d",&n);
+ for(int i=1;i<=n;i++){
+     for(int j=1;j<=i;j++){
+         printf("*");
+         
+     }
+     printf("\n");
+ }
+    return 0;
+}
