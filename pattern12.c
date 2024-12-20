685b70b114a2997285f274d1eaaa773d55f1ae9b
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-12-20 15:19:34 +0530

    Create pattern12.c

---
+++ pattern12.c
@@ -0,0 +1,37 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+   
+    scanf("%d", &n); 
+
+    for (int i = n; i >=1; i--) {
+     for(int j=1;j<=i;j++){
+         printf("*\t");
+     }
+     for(int k=1;k<=(2*(n-i));k++){
+         printf("\t");
+     }
+      for(int m=1;m<=i;m++){
+         printf("*\t");
+     }
+printf("\n");
+}
+
+
+    for (int i = 1; i<=n; i++) {
+     for(int j=1;j<=i;j++){
+         printf("*\t");
+     }
+     for(int k=1;k<=(2*(n-i));k++){
+         printf("\t");
+     }
+      for(int m=1;m<=i;m++){
+         printf("*\t");
+     }
+printf("\n");
+}
+
+    return 0;
+}
